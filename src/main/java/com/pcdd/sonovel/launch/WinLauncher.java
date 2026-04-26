53bbeaf9dfde13553ca0293e96070243566c455f
HaiDianFirstTeam <HaiDianFirstG@outlook.com>
2026-04-26 16:59:57 +0800

    update:26.4.26.16.59

--- src/main/java/com/pcdd/sonovel/launch/WinLauncher.java
+++ src/main/java/com/pcdd/sonovel/launch/WinLauncher.java
@@ -7,6 +7,7 @@ import com.pcdd.sonovel.web.WebServer;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.InetAddress;
